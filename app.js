7f9ed29adb06058157d25eadcb2c075eb0358c52
Ellexandrei Esponilla <xandrei37@gmail.com>
2023-03-12 13:55:02 +0800

    Added logic for drawing, picking color and erasing

---
+++ app.js
@@ -0,0 +1,49 @@
+let COLOR = "#0008ff";
+let ERASE = false;
+
+// Makes the grid
+const sketchContainer = document.querySelector(".sketch-container");
+for (let x = 0; x < 256; x++) {
+  const pixel = document.createElement("div");
+  pixel.classList.add("pixel");
+  sketchContainer.appendChild(pixel);
+}
+
+//
+// drawing logic
+const pixel = document.querySelectorAll(".pixel");
+
+mouseDown = false;
+window.onmousedown = () => (mouseDown = true);
+window.onmouseup = () => (mouseDown = false);
+
+pixel.forEach((pixel) => {
+  pixel.addEventListener(
+    "mouseover",
+    () => mouseDown && (pixel.style.backgroundColor = ERASE ? "#ffffff" : COLOR)
+  );
+
+  //
+  // to color the grid on first click
+  pixel.addEventListener(
+    "mousedown",
+    () => (pixel.style.backgroundColor = ERASE ? "#ffffff" : COLOR)
+  );
+});
+
+//
+// Pick new color
+function setColor(pickedColor) {
+  COLOR = pickedColor;
+}
+const colorPicker = document.getElementById("color-picker");
+colorPicker.oninput = (e) => setColor(e.target.value);
+
+//
+// Erase button
+const eraser = document.getElementById("eraser");
+eraser.addEventListener("click", () => {
+  eraser.classList.toggle("btn-on");
+  ERASE ? (ERASE = false) : (ERASE = true);
+  console.log(ERASE);
+});
